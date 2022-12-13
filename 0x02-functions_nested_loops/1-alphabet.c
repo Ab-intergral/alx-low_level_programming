17cf86639ac0f538b2e9361bad44076614cbe8a4
Ab-intergral <spagsoo@gmail.com>
2022-12-13 14:32:13 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints in lower case
+ */
+
+void print_alphabet(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
