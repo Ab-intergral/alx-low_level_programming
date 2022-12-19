686706f2d001479e65726894f81b0734129b1b30
Ab-intergral <spagsoo@gmail.com>
2022-12-19 14:14:25 -0800

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps values of two integers
+ * @a: integer a
+ * @b: integer b
+ */
+void swap_int(int *a, int *b);
+{
+	int q;
+
+	q = *a;
+	*a = *b;
+	*b = q;
+}
